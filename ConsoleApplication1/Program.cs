9382d2b8bde7a7a89c5a3893d829dbc16997b50d
Dave Mateer <davemateer@gmail.com>
2016-01-05 17:02:40 +0000

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.ReadLine();
+        }
+
+        // 111
+        // 21
+        public void Given3pence_ShouldReturn2DifferentWaysToGetThatAmount()
+        {
+
+        }
+
+        //1111
+        //22
+        //211
+        public void Given4pence_ShouldReturn3DifferentWaysToGetThatAmount()
+        {
+
+        }
+
+        //11111
+        //221
+        //2111
+        //5
+        public void Given5pence_ShouldReturn4DifferentWaysToGetThatAmount()
+        {
+
+        }
+    }
+}
